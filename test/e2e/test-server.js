fe875941498a94f6f9445f49b0c0cad8ad491e0c
ZHAO Xudong <zxdong@gmail.com>
2016-05-06 12:58:43 +0800

    use nightwatch test

---
+++ test/e2e/test-server.js
@@ -0,0 +1,30 @@
+'use strict'
+
+let config0 = require('./config')
+,port = config0.port
+,pack = require('../../package.json')
+let path = require('path')
+let co = require('co')
+let config = require('../../node_modules/jade-press/config-sample')
+config.setting.theme = {
+	path: path.resolve(__dirname, '../..')
+	,name: 'pi'
+	,version: pack.version
+}
+config.setting.plugins = {}
+config.setting.mongoStoreOptions.url = 'mongodb://127.0.0.1:27017/test'
+config.setting.dbLink = 'mongodb://127.0.0.1:27017/test'
+config.local.port = port
+
+let init = require('jade-press').init
+
+co(init(config))
+.then(function(app) {
+
+	app.listen(port, config.setting.listenAddress, function() {
+		console.log('' + new Date(), config.local.siteName, 'runs on port', port)
+	})
+	
+}, function(err) {
+	console.error(err.stack || err)
+})
